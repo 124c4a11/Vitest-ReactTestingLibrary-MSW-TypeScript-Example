b6500d31d67015aade77e5c105017f50d0fd2c02
124c4a11 <124c4a11@mail.ru>
2024-10-12 19:36:00 +0300

    setup testing tools

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [todos, setTodos] = useState([]);
+
+
+  useEffect(() => {
+    fetch("https://dummyjson.com/todos")
+      .then((res) => res.json())
+      .then((res) => setTodos(res.todos));
+  }, []); 
 
   return (
     <>
@@ -21,6 +29,7 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
+        {todos.length && <p>Todo List : {todos.length}</p>}
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
